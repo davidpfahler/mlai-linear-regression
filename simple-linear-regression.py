c008a53eac6056081cd2b08afb36b7d9dff8117e
David Pfahler <djpfahler@gmail.com>
2018-01-17 09:14:59 +0100

    feat: apply to Swedish insurance dataset in python

--- simple-linear-regression.py
+++ simple-linear-regression.py
@@ -1,10 +1,13 @@
 # Port of my previous implementation in JavaScript
 import os.path
 import urllib
+from math import sqrt
+from random import seed
+from random import randrange
 
 # Download dataset if it is not yet present locally.
 filename = 'dataset.csv'
-url = 'http://www.math.muni.cz/~kolacek/docs/frvs/M7222/data/AutoInsurSweden.txt'
+url = 'https://www.math.muni.cz/~kolacek/docs/frvs/M7222/data/AutoInsurSweden.txt'
 
 # replace European style comma with dot notation
 def convert_line (line):
@@ -33,9 +36,46 @@ def variance (values, mean):
 def covariance (x, mean_x, y, mean_y):
   return sum([(x[i] - mean_x) * (y[i] - mean_y) for i in range(len(x))])
 
-dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
-x = [row[0] for row in dataset]
-y = [row[1] for row in dataset]
-mean_x, mean_y = mean(x), mean(y)
-covar = covariance(x, mean_x, y, mean_y)
-print('Covariance: %.3f' % (covar))
+def coefficients (dataset):
+  x = [row[0] for row in dataset]
+  y = [row[1] for row in dataset]
+  mean_x, mean_y = mean(x), mean(y)
+  m = covariance(x, mean_x, y, mean_y) / variance(x, mean_x)
+  t = mean_y - m * mean_x
+  return [t, m]
+
+def simple_linear_regression (train, test):
+  t, m = coefficients(train)
+  return [m * row[0] + t for row in test]
+
+def rmse (actual, predicted):
+  sum_error = 0.0
+  for i in  range(len(actual)):
+    prediction_error = predicted[i] - actual[i]
+    sum_error += prediction_error**2
+  return sqrt(sum_error / len(actual))
+
+def evaluate_algorithm (train, test, algorithm):
+  test_set = [[row[0]] for row in test]
+  predicted = algorithm(train, test_set)
+  actual = [row[1] for row in test]
+  return rmse(actual, predicted)
+
+def split (dataset, factor):
+  train_size = len(dataset) * factor
+  train = list()
+  test = list(dataset)
+  while len(train) < train_size:
+    index = randrange(len(test))
+    train.append(test.pop(index))
+  return [train, test]
+
+def load_dataset (filename):
+  with open(filename) as file:
+    return [map(lambda x: float(x), line.strip('\n').split(';')) for line in file]
+
+seed(1)
+dataset = load_dataset(filename)
+train, test = split(dataset, 0.6)
+rmserr = evaluate_algorithm(train, test, simple_linear_regression)
+print('RMSE: %.3f' % (rmserr))
