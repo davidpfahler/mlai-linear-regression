06eabd63561b0ce0e94f46b783f709dbac943af2
David Pfahler <djpfahler@gmail.com>
2018-01-17 08:43:03 +0100

    feat: covariance in python

--- simple-linear-regression.py
+++ simple-linear-regression.py
@@ -30,10 +30,12 @@ def mean (values):
 def variance (values, mean):
   return sum([(x - mean)**2 for x in values])
 
+def covariance (x, mean_x, y, mean_y):
+  return sum([(x[i] - mean_x) * (y[i] - mean_y) for i in range(len(x))])
+
 dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
 x = [row[0] for row in dataset]
 y = [row[1] for row in dataset]
 mean_x, mean_y = mean(x), mean(y)
-var_x, var_y = variance(x, mean_x), variance(y, mean_y)
-print('for x: mean=%.3f variance=%.3f' % (mean_x, var_x))
-print('for y: mean=%.3f variance=%.3f' % (mean_y, var_y))
+covar = covariance(x, mean_x, y, mean_y)
+print('Covariance: %.3f' % (covar))
