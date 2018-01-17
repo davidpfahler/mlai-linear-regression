c1fbf28cb209081dc25434567d9cbbcb4adbd08a
David Pfahler <djpfahler@gmail.com>
2018-01-17 08:25:36 +0100

    feat: mean & variance in python

--- simple-linear-regression.py
+++ simple-linear-regression.py
@@ -23,3 +23,17 @@ if not os.path.isfile(filename):
     file.write(x + ';' + y)
 
   file.close()
+
+def mean (values):
+  return sum(values) / float(len(values))
+
+def variance (values, mean):
+  return sum([(x - mean)**2 for x in values])
+
+dataset = [[1, 1], [2, 3], [4, 3], [3, 2], [5, 5]]
+x = [row[0] for row in dataset]
+y = [row[1] for row in dataset]
+mean_x, mean_y = mean(x), mean(y)
+var_x, var_y = variance(x, mean_x), variance(y, mean_y)
+print('for x: mean=%.3f variance=%.3f' % (mean_x, var_x))
+print('for y: mean=%.3f variance=%.3f' % (mean_y, var_y))
